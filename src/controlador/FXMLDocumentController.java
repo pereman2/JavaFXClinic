78aeaf7a325ec417d730717b79b1e37161f4bd84
pereman2 <pere-altea@hotmail.com>
2019-04-02 20:47:07 +0200

    Revert ""
    This reverts commit c211047ffa0d7298f34a4bdfe9a6e0acf272e24d.

--- src/controlador/FXMLDocumentController.java
+++ src/controlador/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxclinic.controlador;
+package controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,10 +18,8 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
     
-    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
