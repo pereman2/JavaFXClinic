b9f77ce7ea3a41e3812693fad746cc6e9858a7a2
thecoblack <victoracb13@gmail.com>
2019-04-08 16:47:11 +0200

    Merge origin/master
    Conflicts:
    	src/controlador/FXMLDocumentController.java
    	src/controlador/VentanaAñadirController.java

--- src/controlador/VentanaAñadirController.java
+++ src/controlador/VentanaAñadirController.java
@@ -12,6 +12,7 @@ import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
@@ -46,6 +47,8 @@ public class VentanaAñadirController implements Initializable {
     ObservableList<Patient> pacientes_local;
     ObservableList<Doctor> doctor_local;
     ObservableList<VBox> days;
+    
+    private boolean isDoctor = false;
             
     private String redBackground = "-fx-border-color: red;";
     private String vboxDaysBackground = "-fx-background-color: gray;" + 
@@ -81,28 +84,39 @@ public class VentanaAñadirController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-         db = ClinicDBAccess.getSingletonClinicDBAccess();         
-         field_nombre.setPromptText("Nombre");
-         field_apellidos.setPromptText("Apellidos");
-         field_dni.setPromptText("DNI");
-         field_telefono.setPromptText("Telefono");
-         initImage();
-         initComboBox();
-         initDays();
+        db = ClinicDBAccess.getSingletonClinicDBAccess();         
+        field_nombre.setPromptText("Nombre");
+        field_apellidos.setPromptText("Apellidos");
+        field_dni.setPromptText("DNI");
+        field_telefono.setPromptText("Telefono");
+        initImage();
+        FXMLLoader fmxl = new FXMLLoader(url);
+        if (fmxl.equals(new FXMLLoader(
+                getClass().getResource(
+                "/vista/VentanaAñadirDoctor.fxml")))) {
+            isDoctor = true;
+            initComboBox();
+            initDays();
+        }        
     }    
 
     @FXML
     private void aceptar(ActionEvent event) {
         if (esCorrecto()) {
-            String name = field_nombre.getText();
-            String surname = field_apellidos.getText();
-            String identifier = field_dni.getText();
-            String telephon = field_telefono.getText();
-            Image photo = img.getImage();
-            Patient aux = new Patient(identifier, name, surname, telephon, photo);
-            FXMLDocumentController.getClinicDBAccess().getPatients().add(aux);
-            pacientes_local.add(aux);
-            ((Stage) img.getScene().getWindow()).close();
+            if (isDoctor) {            
+                
+            }
+            else {
+                String name = field_nombre.getText();
+                String surname = field_apellidos.getText();
+                String identifier = field_dni.getText();
+                String telephon = field_telefono.getText();
+                Image photo = img.getImage();
+                Patient aux = new Patient(identifier, name, surname, telephon, photo);
+                FXMLDocumentController.getClinicDBAccess().getPatients().add(aux);
+                pacientes_local.add(aux);
+                ((Stage) img.getScene().getWindow()).close();
+            }
         }
         else {
             Alert alert = new Alert(AlertType.ERROR);
@@ -174,16 +188,17 @@ public class VentanaAñadirController implements Initializable {
     //caracter incorrecto
     private void colorFondoCaracter(KeyEvent event) {        
         TextField id = (TextField) event.getSource();
-        boolean aux;        
+        boolean aux = true;        
         if (id == field_nombre || id == field_apellidos) {
             aux = caracteresCompatibles(id.getText(), NOMBRE_APELLIDOS);
         }
         else if (id == field_dni) {
             aux = caracteresCompatibles(id.getText(), DNI);
         }
-        else {
+        else if (id == field_telefono){
             aux = caracteresCompatibles(id.getText(), TELEFONO);
         }
+        
         
         
         if (!aux) {
